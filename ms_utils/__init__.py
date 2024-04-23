6403a8df267b6cb5d061d5b20665f904cc3011bd
cfs4819 <1294397380@qq.com>
2024-04-23 16:54:05 +0800

    update utils

--- ms_utils/__init__.py
+++ ms_utils/__init__.py
@@ -1,7 +1,7 @@
 import math
 from typing import List
 import numpy as np
-
+# import copy
 # from config import APOLLO_ROOT, RECORDS_DIR, STREAM_LOGGING_LEVEL
 import carla
 
@@ -10,37 +10,43 @@ def calc_relative_loc(ref_tf: carla.Transform,
                       offset_f: float,
                       offset_r: float,
                       offset_h=0.0) -> carla.Location:
-    ref_loc = ref_tf.location
-    ref_f_vec = ref_tf.get_forward_vector()
-    ref_r_vec = ref_tf.get_right_vector()
-    ref_u_vec = ref_tf.get_up_vector()
-    target_x = ref_loc.x + offset_f * ref_f_vec.x + \
-        offset_r * ref_r_vec.x + offset_h * ref_u_vec.x
-    target_y = ref_loc.y + offset_f * ref_f_vec.y + \
-        offset_r * ref_r_vec.y + offset_h * ref_u_vec.y
-    target_z = ref_loc.z + offset_f * ref_f_vec.z + \
-        offset_r * ref_r_vec.z + offset_h * ref_u_vec.z
-    return carla.Location(target_x, target_y, target_z)
+    result_loc = carla.Location(x=offset_f, y=offset_r, z=offset_h)
+    ref_tf.transform(result_loc)
+    return result_loc
+    # ref_loc = ref_tf.location
+    # ref_f_vec = ref_tf.get_forward_vector()
+    # ref_r_vec = ref_tf.get_right_vector()
+    # ref_u_vec = ref_tf.get_up_vector()
+    # target_x = ref_loc.x + offset_f * ref_f_vec.x + \
+    #     offset_r * ref_r_vec.x + offset_h * ref_u_vec.x
+    # target_y = ref_loc.y + offset_f * ref_f_vec.y + \
+    #     offset_r * ref_r_vec.y + offset_h * ref_u_vec.y
+    # target_z = ref_loc.z + offset_f * ref_f_vec.z + \
+    #     offset_r * ref_r_vec.z + offset_h * ref_u_vec.z
+    # return carla.Location(target_x, target_y, target_z)
 
 
 def calc_relative_loc_dict(ref_tf: carla.Transform,
                            offset_f: float,
                            offset_r: float,
                            offset_h=0.0) -> carla.Location:
-    ref_loc = ref_tf.location
-    ref_f_vec = ref_tf.get_forward_vector()
-    ref_r_vec = ref_tf.get_right_vector()
-    ref_u_vec = ref_tf.get_up_vector()
-    target_x = ref_loc.x + offset_f * ref_f_vec.x + \
-        offset_r * ref_r_vec.x + offset_h * ref_u_vec.x
-    target_y = ref_loc.y + offset_f * ref_f_vec.y + \
-        offset_r * ref_r_vec.y + offset_h * ref_u_vec.y
-    target_z = ref_loc.z + offset_f * ref_f_vec.z + \
-        offset_r * ref_r_vec.z + offset_h * ref_u_vec.z
-    return {'x': target_x, 'y': target_y, 'z': target_z}
+    result_loc = carla.Location(x=offset_f, y=offset_r, z=offset_h)
+    ref_tf.transform(result_loc)
+    return {'x': result_loc.x, 'y': result_loc.y, 'z': result_loc.z}
+    # ref_loc = ref_tf.location
+    # ref_f_vec = ref_tf.get_forward_vector()
+    # ref_r_vec = ref_tf.get_right_vector()
+    # ref_u_vec = ref_tf.get_up_vector()
+    # target_x = ref_loc.x + offset_f * ref_f_vec.x + \
+    #     offset_r * ref_r_vec.x + offset_h * ref_u_vec.x
+    # target_y = ref_loc.y + offset_f * ref_f_vec.y + \
+    #     offset_r * ref_r_vec.y + offset_h * ref_u_vec.y
+    # target_z = ref_loc.z + offset_f * ref_f_vec.z + \
+    #     offset_r * ref_r_vec.z + offset_h * ref_u_vec.z
+    # return {'x': target_x, 'y': target_y, 'z': target_z}
 
 
-def inverse_transform_point(local_transform: carla.Transform, point_loc: carla.Location):
+def inverse_transform_point(local_transform: carla.Transform, point_loc: carla.Location) -> carla.Location:
     world_2_local = np.array(local_transform.get_inverse_matrix())
     points = np.array(
         [point_loc.x, point_loc.y, point_loc.z, 1])
