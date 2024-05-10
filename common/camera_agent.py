2596876de8c7b2a0cf38567b3845d88d5dda59e0
cfs4819 <1294397380@qq.com>
2024-05-10 23:05:00 +0800

    Move locations and perform performance optimizations on recorder

--- common/camera_agent.py
+++ common/camera_agent.py
@@ -7,14 +7,29 @@ import datetime
 from carla import ColorConverter as cc
 import os
 
+# import pygame
+
 
 class ScenarioRecorder:
-    def __init__(self,
-                 world: carla.World,
-                 ego_vehicle: carla.Vehicle,
-                 save_floder_path,
-                 resolution=(1280, 720),
-                 frame_rate=24.0):
+    def __init__(self, world: carla.World, ego_vehicle: carla.Vehicle, save_floder_path, resolution=(832,468), frame_rate=24.0):
+        """
+        Initializes the ScenarioRecorder class to record scenarios in the CARLA simulator.
+
+        Parameters:
+        - world (carla.World): The simulation world.
+        - ego_vehicle (carla.Vehicle): The vehicle being controlled or observed in the simulation.
+        - save_folder_path (str): Directory path for saving the recordings.
+        - resolution (tuple): Image resolution for recording, default (832,468). Choose from
+            [   320x180,    384x216, 
+                448x252,    512x288, 
+                576x324,    640x360, 
+                704x396,    768x432, 
+                832x468,    896x504, 
+                960x540,    1024x576    
+                and those larger then 1280x720
+            ].
+        - frame_rate (float): Recording frame rate in frames per second, default 24.0.
+        """
         self.world = world
         self.ego_vehicle = ego_vehicle
         self.save_path = save_floder_path
@@ -34,78 +49,41 @@ class ScenarioRecorder:
         self.back_cam_tf = carla.Transform(carla.Location(x=-0.8, y=0.0, z=1.8),
                                            carla.Rotation(pitch=0, yaw=-180, roll=0))
 
-        self.cam_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
-        self.cam_bp.set_attribute('image_size_x', str(self.sub_fig_res[0]))
-        self.cam_bp.set_attribute('image_size_y', str(self.sub_fig_res[1]))
-        self.cam_bp.set_attribute('sensor_tick', str(1.0 / self.frame_rate))
-
-        self.cam_bp.set_attribute('role_name', 'recorder_top_cam')
-        self.top_cam = self.world.spawn_actor(self.cam_bp,
-                                              self.top_cam_tf,
-                                              attach_to=self.ego_vehicle)
-        self.world.wait_for_tick()
-
-        self.cam_bp.set_attribute('role_name', 'recorder_tpp_cam')
-        self.tpp_cam = self.world.spawn_actor(self.cam_bp,
-                                              self.tpp_cam_tf,
-                                              attach_to=self.ego_vehicle,
-                                              attachment_type=carla.AttachmentType.SpringArmGhost)
-        self.world.wait_for_tick()
-
-        self.cam_bp.set_attribute('role_name', 'recorder_fpp_cam')
-        self.fpp_cam = self.world.spawn_actor(self.cam_bp,
-                                              self.fpp_cam_tf,
-                                              attach_to=self.ego_vehicle)
-        self.world.wait_for_tick()
-
-        self.cam_bp.set_attribute('role_name', 'recorder_back_cam')
-        self.back_cam = self.world.spawn_actor(self.cam_bp,
-                                               self.back_cam_tf,
-                                               attach_to=self.ego_vehicle)
-        self.world.wait_for_tick()
+        self.top_cam = self.create_camera(self.top_cam_tf, 'recorder_top_cam')
+        self.tpp_cam = self.create_camera(self.tpp_cam_tf, 'recorder_tpp_cam', carla.AttachmentType.SpringArmGhost)
+        self.fpp_cam = self.create_camera(self.fpp_cam_tf, 'recorder_fpp_cam')
+        self.back_cam = self.create_camera(self.back_cam_tf, 'recorder_back_cam')
 
         self.video_writer: cv2.VideoWriter = None
         self.stop_event = threading.Event()  # Initialize the stop event
         self.recording_thread: threading.Thread = None
 
         self.width, self.height = self.sub_fig_res
+        self.sensor_frame = [None,None,None,None] # [top_frame, tpp_frame, fpp_frame, back_frame]
         self.recording_frame = np.zeros(
             (self.height * 2, self.width * 2, 3), dtype=np.uint8)
 
-    def top_img_callback(self, image):
-        # Convert image to an array for video recording
-        top_array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-        top_array = np.reshape(top_array, (image.height, image.width, 4))
-        top_array = top_array[:, :, :3]     # Remove alpha channel
+    def create_camera(self, transform, role_name, attachment_type=carla.AttachmentType.Rigid):
+        cam_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
+        cam_bp.set_attribute('image_size_x', str(self.sub_fig_res[0]))
+        cam_bp.set_attribute('image_size_y', str(self.sub_fig_res[1]))
+        cam_bp.set_attribute('sensor_tick', str(1.0 / self.frame_rate))
+        cam_bp.set_attribute('role_name', role_name)
+        camera = self.world.spawn_actor(cam_bp, transform, attach_to=self.ego_vehicle, attachment_type=attachment_type)
+        self.world.wait_for_tick()
+        return camera
 
-        self.recording_frame[:self.height, :self.width, :] = top_array
+    def top_img_callback(self, image):
+        self.sensor_frame[0] = image
+
 
     def tpp_img_callback(self, image):
-        # Convert image to an array for video recording
-        tpp_array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-        tpp_array = np.reshape(tpp_array, (image.height, image.width, 4))
-        tpp_array = tpp_array[:, :, :3]     # Remove alpha channel
-
-        self.recording_frame[:self.height,
-                             self.width:self.width*2, :] = tpp_array
+        self.sensor_frame[1] = image
 
     def fpp_img_callback(self, image):
-        # Convert image to an array for video recording
-        fpp_array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-        fpp_array = np.reshape(fpp_array, (image.height, image.width, 4))
-        fpp_array = fpp_array[:, :, :3]     # Remove alpha channel
-
-        self.recording_frame[self.height:self.height *
-                             2, :self.width, :] = fpp_array
-
+        self.sensor_frame[2] = image
     def back_img_callback(self, image):
-        # Convert image to an array for video recording
-        back_array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-        back_array = np.reshape(back_array, (image.height, image.width, 4))
-        back_array = back_array[:, :, :3]     # Remove alpha channel
-
-        self.recording_frame[self.height:self.height *
-                             2, self.width:self.width*2, :] = back_array
+        self.sensor_frame[3] = image
 
     def start_recording(self, save_path=None):
         self.recording_frame = np.zeros(
@@ -117,7 +95,6 @@ class ScenarioRecorder:
         self.tpp_cam.listen(self.tpp_img_callback)
         self.fpp_cam.listen(self.fpp_img_callback)
         self.back_cam.listen(self.back_img_callback)
-
         if save_path == None:
             if not os.path.exists(self.save_path):
                 os.makedirs(self.save_path)
@@ -136,12 +113,41 @@ class ScenarioRecorder:
         self.recording_thread = threading.Thread(
             target=self.recording_thread_handler)
         self.recording_thread.start()
-
     def recording_thread_handler(self):
+        self.stop_event.clear()
         while not self.stop_event.is_set():
+            if any(frame == None for frame in self.sensor_frame):
+                continue
+
+            start_time = time.time()
+
+            record_array = np.empty((4, self.height, self.width, 3), dtype=np.uint8)
+
+            for index, frame in enumerate(self.sensor_frame):
+                # print(f'{frame.width}x{frame.height}')
+                array = np.frombuffer(frame.raw_data, dtype=np.dtype("uint8")).reshape((frame.height, frame.width, 4))
+                # Convert image to an array for video recording
+                record_array[index] = array[:, :, :3]
+
+            self.recording_frame[:self.height, :self.width, :] = record_array[0]
+            self.recording_frame[self.height:self.height*2, :self.width, :] = record_array[1]
+            self.recording_frame[:self.height, self.width:self.width*2, :] = record_array[2]
+            self.recording_frame[self.height:self.height*2, self.width:self.width*2, :] = record_array[3]
+
             self.video_writer.write(self.recording_frame)
 
-            time.sleep(1.0 / self.frame_rate)
+            elapsed_time = time.time() - start_time
+            sleep_time = max(0, (1.0 / self.frame_rate) - elapsed_time)
+            time.sleep(sleep_time)
+        # array = np.frombuffer(self.sensor_frame[0].raw_data, dtype=np.dtype("uint8"))
+        # print(array.shape)
+        # array = array.reshape((self.sensor_frame[0].height, self.sensor_frame[0].width, 4))
+        # print(array.shape)
+        # print(self.sensor_frame[0].height*self.sensor_frame[0].width*4)
+        # # CARLA's images are BGRA, not RGB
+        # bgra_image = array[:, :, :4]  # Include alpha for saving to file
+        # rgb_image = cv2.cvtColor(bgra_image, cv2.COLOR_BGRA2RGB)
+        # cv2.imwrite('output_image.png', rgb_image)
 
     def stop_recording(self):
         self.stop_event.set()
@@ -173,14 +179,21 @@ if __name__ == '__main__':
     time.sleep(1)
 
     ego_vehicle = None
+    spawn_one = False
     for actor in world.get_actors().filter('vehicle.*'):
         # print(actor.attributes.get('role_name'))
         if actor.attributes.get('role_name') in ['hero', 'ego_vehicle']:
             ego_vehicle = actor
             break
     if ego_vehicle == None:
-        print("No ego vehicle found")
-        exit()
+        print("No ego vehicle found, spawning one")
+        bps = world.get_blueprint_library().filter('vehicle.tesla.model3')
+    
+        ego_tf = world.get_map().get_spawn_points()[0]
+        ego_bp = bps[0]
+        ego_bp.set_attribute('role_name', 'ego_vehicle')
+        ego_vehicle = world.try_spawn_actor(ego_bp, ego_tf)
+        spawn_one = True
     print("Ego vehicle found")
 
     recorder = ScenarioRecorder(world, ego_vehicle, f'./save')
@@ -191,7 +204,7 @@ if __name__ == '__main__':
         time.sleep(0.1)
     recorder.stop_recording()
     print("Recording stopped")
-    
+
     print("Starting second recording, will record for 20 seconds")
     recorder.start_recording()
     start = time.time()
@@ -199,3 +212,6 @@ if __name__ == '__main__':
         time.sleep(0.1)
     recorder.stop_recording()
     print("Recording stopped")
+    
+    if spawn_one:
+        ego_vehicle.destroy()
