2ef10eafb11e08fe4934c4f05afa4bd6b063b6f9
lshabaddon@163.com <lshabaddon@163.com>
2025-01-15 15:18:35 +0800

    fix pic of plotly

--- planning/DStar.py
+++ planning/DStar.py
@@ -350,6 +350,26 @@ def save_grid_plotly(carla_map, ego_vehicle, lane_grid, obs, target_grid, path, 
         width=800,
         height=800
     )
+    fig.update_layout(
+        xaxis=dict(
+            title="X (meters)",
+            showgrid=False,
+            zeroline=False,
+            showline=True,
+            linecolor='black',
+            linewidth=1
+        ),
+        yaxis=dict(
+            title="Y (meters)",
+            showgrid=False,
+            zeroline=False,
+            showline=True,
+            linecolor='black',
+            linewidth=1
+        ),
+        plot_bgcolor='white',
+        paper_bgcolor='white'
+    )
 
     # Save the figure as a static image
     fig.write_image(filename)
